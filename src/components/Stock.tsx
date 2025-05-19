374dc53ada86d8e6d6b8f41fdcf91887ce71f63c
Amber Welby <amberwelby@trentu.ca>
2025-05-18 21:57:32 -0400

    Create Stock and Stock Grid components

---
+++ src/components/Stock.tsx
@@ -0,0 +1,34 @@
+import './Stock.css'
+
+export function Stock({
+    ticker, 
+    shares,
+}:{ticker: string,
+    shares: number,
+}){
+    //temp
+    const name: string = "Topicus.com"
+    const pps: number = 999;
+    const totalValue: number = pps * shares;
+    const totalSpent: number = 888;
+    const gainLoss: number = totalValue - totalSpent;
+
+    return(
+        <div className="stock">
+            <h3>{name}: {ticker}</h3>
+            <p>Shares: {shares}</p>
+            <p>Price/Share: {pps}</p>
+            <p className={gainLoss > 0 ? 'gain' : 'loss'}>Gain/Loss: {gainLoss}</p>
+        </div>
+    )
+}
+
+/* What I want to know 
+    Ticker (display, store)
+    Name (display)
+    No. Shares (display, store)
+    Price per Share (display)
+    Total value (display)
+    Total amount spent to purchase (store)
+    Total gain/loss (display)
+*/
---
+++ src/components/StockGrid.tsx
@@ -0,0 +1,15 @@
+import './StockGrid.css'
+import { Stock } from "./Stock"
+
+export function StockGrid(){
+    return(
+        <div className='grid'>
+            <Stock ticker={"TOI"} shares={1}/>
+            <Stock ticker={"TOI"} shares={1}/>
+            <Stock ticker={"TOI"} shares={1}/>
+            <Stock ticker={"TOI"} shares={1}/>
+            <Stock ticker={"TOI"} shares={1}/>
+
+        </div>
+    )
+}
