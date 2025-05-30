cf02d162477745b4654dedb9dd19b959718c22e8
Amber Welby <amberwelby@trentu.ca>
2025-05-29 23:14:52 -0400

    Customize weather component with json config file

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import './App.css'
 import { TodaysDate } from './components/TodaysDate'
 import { StockGrid } from './components/StockGrid'
 import { WeatherCarousel, type City } from './components/WeatherCarousel'
+import userConfig from '../duckyconfig-sample.json' with { type: 'json' };
 
 function App() {
 
-  const testCities : City[] = [
-    {name: "Ottawa, ON", lat: "45.4201", long: "-75.7003"},
-    {name: "Peterborough, ON", lat: "44.301111", long: "-78.333333"}
-]
+  const userCities : City[] = userConfig.weather;
+
+//   const testCities : City[] = [
+//     {name: "Ottawa, ON", lat: "45.4201", long: "-75.7003"},
+//     {name: "Peterborough, ON", lat: "44.301111", long: "-78.333333"}
+// ]
 
   return (
     <>
       <div className='cols'>
         <div className='rightCol'>
           <TodaysDate/>
-          <WeatherCarousel cities={testCities} />
+          <WeatherCarousel cities={userCities} />
         </div>
           <StockGrid/>
       </div>
